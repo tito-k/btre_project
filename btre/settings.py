66157857a0184c80f380017992a7d46f56425162
tito-k <31657846+tito-k@users.noreply.github.com>
2021-01-28 18:35:07 +0100

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -145,6 +145,6 @@ MESSAGE_TAGS = {
 # Email Config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER='komolafetoyin17@gmail.com'
-EMAIL_HOST_PASSWORD='chodnofsky'
+EMAIL_HOST_USER='komolafetoyin17@gmail.com' #use your email
+EMAIL_HOST_PASSWORD='*************' #use your email password
 EMAIL_USE_TLS=True
